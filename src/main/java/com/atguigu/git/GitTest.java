b8824b232814ec9176c2cb28d654b9a0c8bf39d7
zsx <1239625713@qq.com>
2022-06-02 11:32:18 +0800

    dev的添加

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("第一次更新");
+        System.out.println("dev的添加");
     }
 }
