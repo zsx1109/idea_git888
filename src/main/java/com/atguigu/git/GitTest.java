96f437f3f20946db86d9f54df3cab07c63433c4d
zsx <1239625713@qq.com>
2022-06-02 10:04:45 +0800

    新建

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * Date:2022/6/2
+ * Author:zsx
+ * Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("新建");
+    }
+}
